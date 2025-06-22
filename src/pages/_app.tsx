871768ccd0a83a361a18fe910eb3116b95d5e66e
Vijay <jagdalevijay92@gmail.com>
2025-06-23 01:17:05 +0530

    feat: update website title and metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 
 import "@/styles/globals.css";
 import "@/styles/locomotive-scroll.css";
@@ -13,6 +14,10 @@ const dmSans = DM_Sans({
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div lang={"en"} className={dmSans.className}>
+      <Head>
+        <title>Vijay Jagdale - Portfolio</title>
+        <meta name="description" content="The portfolio of Vijay Jagdale, a software engineer specializing in Java, Python, and machine learning." />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
